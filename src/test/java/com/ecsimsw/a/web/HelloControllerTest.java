c2e5753c6774478bbe029cb2f86e77e3d031717f
ecsimsw <ecsimsw@gmail.com>
2020-03-03 17:48:28 +0900

    test code

--- src/test/java/com/ecsimsw/a/web/HelloControllerTest.java
+++ src/test/java/com/ecsimsw/a/web/HelloControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = HelloController.class)
 //스프링 테스트 어노테이션 중, spring MVC에 집중할 수 있도록 한다.
 
-public class HelloControllerTest{
+public class HelloControllerTest {
     @Autowired
     // 스프링이 관리하는 Bean을 주입한다.
 
