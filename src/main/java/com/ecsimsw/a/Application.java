515ba49a5842e6dfd0c41995f9dbe58fd6f30d39
seflmsw <ecsimsw@gamil.com>
2020-03-23 04:49:45 +0900

    Bean Configuration

--- src/main/java/com/ecsimsw/a/Application.java
+++ src/main/java/com/ecsimsw/a/Application.java
@@ -1,17 +1,25 @@
 package com.ecsimsw.a;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
 public class Application {
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
-    }
+        //Bean Configuration by Java _ ApplicationConfig
+        AnnotationConfigApplicationContext context =
+           new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-    @Bean
-    public String jinhwan(){
-        return "jinhwan";
+        Person person = (Person)context.getBean(Person.class);
+        person.say();
+
+
+
+        //SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/com/ecsimsw/a/Body.java
@@ -0,0 +1,6 @@
+package com.ecsimsw.a;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Body {
+}
