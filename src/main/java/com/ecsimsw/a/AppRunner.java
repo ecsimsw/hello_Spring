ff4ceba622932571ae94e0d56e1313d29c0f24b2
seflmsw <ecsimsw@gamil.com>
2020-03-29 03:47:42 +0900

    MessageSource / messages.properties

--- src/main/java/com/ecsimsw/a/AppRunner.java
+++ src/main/java/com/ecsimsw/a/AppRunner.java
@@ -3,20 +3,28 @@ package com.ecsimsw.a;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    Environment environment;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        String item = environment.getProperty("item");
-        System.out.println(item);
-    }
+        System.out.println(messageSource.getClass());
 
-    @Autowired
-    Person person;
+        System.out.println("== Greeting == ");
+        System.out.println(messageSource.getMessage("greet",null, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greet",null, Locale.US));
+
+        System.out.println("== login == ");
+        String id = "ecsimsw";
+        System.out.println(messageSource.getMessage("login", new String[]{id}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("login", new String[]{id}, Locale.US));
+    }
 }
