a009df5bd2e01ef72d5b58f9f9cf6b2d8edc168a
seflmsw <ecsimsw@gmail.com>
2020-03-29 03:47:42 +0900

    MessageSource / messages.properties

--- src/main/java/com/ecsimsw/a/Proto.java
+++
@@ -1,8 +0,0 @@
-package com.ecsimsw.a;
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component @Scope("prototype")
-public class Proto {
-}
