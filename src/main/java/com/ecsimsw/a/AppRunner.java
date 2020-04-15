af38ecb0e5809de8f6197a33f4cafd588c8fd7f1
seflmsw <ecsimsw@gmail.com>
2020-04-15 17:22:08 +0900

    ResourceLoader

--- src/main/java/com/ecsimsw/a/AppRunner.java
+++ src/main/java/com/ecsimsw/a/AppRunner.java
@@ -2,27 +2,38 @@ package com.ecsimsw.a;
 
 import com.ecsimsw.a.applicationEventPublisher.Event;
 import com.ecsimsw.a.resourceLoader.RL;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class AppRunner implements org.springframework.boot.ApplicationRunner {
 
     @Autowired
-    org.springframework.core.io.ResourceLoader resourceLoader;
+    ResourceLoader resourceLoader;
 
     @Autowired
     ApplicationContext ctx;
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        RL rl = new RL(ctx);
-        ctx.publishEvent(new Event("sdf",0));
+        System.out.println(resourceLoader.getClass());
+
+        var ctx = new ClassPathXmlApplicationContext();
+        System.out.println(ctx.getClass());
+
+
+        Resource resource_classPath = resourceLoader.getResource("classpath:ecsimsw.txt");
+        System.out.println(resource_classPath.getClass());
+        System.out.println(resource_classPath.exists());
+        System.out.println(resource_classPath.getDescription());
     }
 }
--- src/main/java/com/ecsimsw/a/Application.java
+++ src/main/java/com/ecsimsw/a/Application.java
@@ -2,7 +2,7 @@ package com.ecsimsw.a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
