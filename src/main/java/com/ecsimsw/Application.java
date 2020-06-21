1e7a3d504bc6c0ad2024619b4a6b641d69ba588a
ecsimsw <ecsimsw@gmail.com>
2020-06-21 18:21:09 +0900

    2020-06-21 / resource 다루기

--- src/main/java/com/ecsimsw/Application.java
+++ src/main/java/com/ecsimsw/Application.java
@@ -1,7 +1,9 @@
 package com.ecsimsw;
 
+import java.io.IOException;
 import java.util.Arrays;
 
+import com.sun.org.apache.xml.internal.utils.res.XResourceBundle;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,27 +14,34 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        ApplicationContext ctx =
-                new AnnotationConfigApplicationContext(ApplicationConfigure.class);
+ApplicationContext ctx = new ClassPathXmlApplicationContext();
 
-        Proto proto1 = ctx.getBean(Proto.class);
-        Proto proto2 = ctx.getBean(Proto.class);
+Resource resourceClassPath = ctx.getResource("testFile.txt");
+System.out.println(resourceClassPath.getClass());
+System.out.println(resourceClassPath.getURI().getPath());
+System.out.println(resourceClassPath.exists());
 
-        Singleton singleton1 = ctx.getBean(Singleton.class);
-        Singleton singleton2 = ctx.getBean(Singleton.class);
+System.out.println();
 
-        System.out.println(proto1.hashCode());
-        System.out.println(proto1.getProto2());
-        System.out.println(proto2.hashCode());
-        System.out.println(proto2.getProto2());
-        System.out.println(singleton1.hashCode());
-        System.out.println(singleton1.getProto2());
-        System.out.println(singleton2.hashCode());
-        System.out.println(singleton2.getProto2());
+Resource resourceFilePath = ctx.getResource("file:///C:/Users/luraw/git/hello_Spring/build/resources/main/testFile.txt");
+System.out.println(resourceFilePath.getClass());
+System.out.println(resourceFilePath.getURI().getPath());
+System.out.println(resourceFilePath.exists());
+
+
+        // resource의 타입은 location 문자열과 ApplicationContext의 타입에 따라 결정된다.
+        // ClassPathXmlApplicationContext -> ClassPathResource
+        // FileSystemXmlApplicationContext -> FileSystemResource
+        // WebApplicationContext -> ServletContextResource
+        // 대부분의 경우 WebApplicationContext를 사용하기때문에 ServletContextResource를 사용한다.
+
+        // 또는 prefix를 사용해서 강제할 수 도 있다.
+        // classpath: 와 file:
     }
 }
