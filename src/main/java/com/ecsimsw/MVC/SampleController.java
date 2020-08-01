233282ca4edb395badd9b73c7fb10488c3984036
ecsimsw <ecsimsw@gmail.com>
2020-08-02 06:09:44 +0900

    request mapping / MockMvc
    Uri, header, param, mediaType

--- src/main/java/com/ecsimsw/MVC/SampleController.java
+++ src/main/java/com/ecsimsw/MVC/SampleController.java
@@ -1,24 +1,29 @@
 package com.ecsimsw.MVC;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping() // 클래스 안 모든 핸들러에 적용한다.
+@RestController
 public class SampleController {
-
-    // Method를 지정하지 않으면 모든 http method를 다 받음
-    // GET, POST, PUT, PATCH, DELETE
-    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(){
+    @RequestMapping(value="hello", headers = HttpHeaders.AGE)
+    @PostMapping
+    public String Test1(){
+        return "hello";
+    }
+    @RequestMapping(value="hello", headers = "!"+HttpHeaders.AGE)
+    public String Test2(){
+        return "hello";
+    }
+    @RequestMapping(value="hello", headers = HttpHeaders.AGE+"=111")
+    public String Test3(){
         return "hello";
     }
 
-    // GET : 서버 리소스 요청, idempotent
-    // POST : 서버의 리소스 수정, 생성
-    // PUT : URI는 보내는 데이터에 해당하는 리소스를 지칭, idempotent
-    // PATCH : 기존 엔티티와 새 테이터의 차이점만 보냄, idempotent
-    // DELETE : URI에 해당하는 리소스 삭제 요청, idempotent
+    @RequestMapping(value="hello", params = "name=test")
+    public String Test4(){
+        return "hello";
+    }
 }
 
--- src/test/java/com/ecsimsw/MVC/SampleControllerTest.java
+++ src/test/java/com/ecsimsw/MVC/SampleControllerTest.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -22,9 +23,14 @@ public class SampleControllerTest{
 
     @Test
     public void hello() throws Exception{
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(
+                get("/hello")
+                .header(HttpHeaders.AGE,"111")
+                .param("name","test")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
+                .andExpect(content().string("hello"))
+                .andExpect(handler().handlerType(SampleController.class));
     }
 }
