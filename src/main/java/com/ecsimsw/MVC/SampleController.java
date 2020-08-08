e7ad2a08cab6422318f2e0c0d016b55c830742c6
ecsimsw <ecsimsw@gmail.com>
2020-08-09 04:25:21 +0900

    redirectAttributes / flashAttribute

--- src/main/java/com/ecsimsw/MVC/SampleController.java
+++ src/main/java/com/ecsimsw/MVC/SampleController.java
@@ -3,16 +3,18 @@ package com.ecsimsw.MVC;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 @SessionAttributes("memberId")
 public class SampleController {
     @GetMapping(value="/login")
@@ -26,6 +28,20 @@ public class SampleController {
     public String complete(SessionStatus sessionStatus){
         sessionStatus.setComplete();
         return "hello";
+    }
+
+@GetMapping(value="/hello")
+public String hello(RedirectAttributes redirectAttributes, Model model){
+    //redirectAttributes.addAttribute("name", "jinhwan");
+    redirectAttributes.addFlashAttribute("name","jinhwan");
+    return "redirect:/redirected";
+}
+
+    @GetMapping(value="/redirected")
+    @ResponseBody
+    public String hi(Model model, @SessionAttribute("name") String name){
+        //String name = (String)model.asMap().get("name");
+        return name;
     }
 }
 
