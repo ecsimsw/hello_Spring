e27e1573f41d6ba9f71e2efc5a81b015f73af938
ecsimsw <ecsimsw@gmail.com>
2021-02-12 23:46:47 +0900

    Servlet 컨테이너, 동작 원리

---
+++ src/main/java/com/ecsimsw/servletExample/HelloServlet.java
@@ -0,0 +1,39 @@
+package com.ecsimsw.servletExample;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello");
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+        super.destroy();
+    }
+}
+
+/*
+ 서블릿 동작 원리
+
+서블릿 컨테이너
+ 1. 웹 서버로부터 요청을 받아 HttpServletRequest 객체와 HttpServletResponse 객체를 생성한다.
+ 2. 요청 URL이 어떤 서블릿이 필요한 요청인지 설정 파일을 참고하여 찾는다. (web.xml)
+ 3. 해당 서블릿 인스턴스가 존재하는지 확인하고 없다면 init()를 생성하여 로딩한다.
+ 4. 스레드를 생성하고 서블릿 인스턴스의 서비스 로직이 실행된다. (doGet, doPost)
+ 5. 응답이 완료되면 HttpServletRequest 객체와 HttpServletResponse 객체를 소멸시킨다.
+  */
