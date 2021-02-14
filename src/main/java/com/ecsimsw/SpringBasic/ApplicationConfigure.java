d214f090b7950172723290b06f586da009e24f81
ecsimsw <ecsimsw@gmail.com>
2021-02-15 00:29:00 +0900

    * What went wrong: Execution failed for task ':compileJava'. > Compilation failed; see the compiler error output for details.

--- src/main/java/com/ecsimsw/SpringBasic/ApplicationConfigure.java
+++
@@ -1,9 +0,0 @@
-package com.ecsimsw.SpringBasic;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = Application.class)
-public class ApplicationConfigure {
-}
